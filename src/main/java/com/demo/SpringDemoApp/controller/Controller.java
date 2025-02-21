2ff363d78167f3074543ad659b5f7c070cb0f755
imprathamraj <rjpratham05@gmail.com>
2025-02-21 23:06:30 +0530

    [Pratham Raj] Add. Make REST Call to show Hello Mark form BridgeLabz Use GET Request Method and pass name as path variable

--- src/main/java/com/demo/SpringDemoApp/controller/Controller.java
+++ src/main/java/com/demo/SpringDemoApp/controller/Controller.java
@@ -13,4 +13,9 @@ public class Controller {
     public String sayHello(@RequestParam(value = "name") String name){
         return "Hello " + name + "!";
     }
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello " + name + "!";
+    }
+
 }
