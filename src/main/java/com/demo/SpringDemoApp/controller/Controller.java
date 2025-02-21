caa72128ec6ab35ca33d48cefa9c28551d35348d
imprathamraj <rjpratham05@gmail.com>
2025-02-21 23:48:57 +0530

    [Pratham Raj] Add. Make REST Call to show Hello Mark form BridgeLabz Use PUT Request Method and pass first name as Path Variable and last name as Query Parameter

--- src/main/java/com/demo/SpringDemoApp/controller/Controller.java
+++ src/main/java/com/demo/SpringDemoApp/controller/Controller.java
@@ -22,5 +22,10 @@ public class Controller {
     public String sayHello(@RequestBody User user){
         return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
     }
+    @PostMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastName") String lastName){
+        return "Hello " + firstName + " " + lastName + "!";
+    }
 
 }
